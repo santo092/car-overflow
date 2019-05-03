532c3dd4b6db10b8d2ee9cf31f7acfcb5e7bad87
Isaiahmasse <47521451+Isaiahmasse@users.noreply.github.com>
2019-05-03 13:02:43 -0700

    sign up/log in page fixed (#18)

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AuthService from './../components/AuthService';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../utils/login.css';
 
 class Login extends Component {
   constructor() {
@@ -29,42 +30,52 @@ class Login extends Component {
   };
 
   handleChange = event => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
     this.setState({
-        [name]: value
+      [name]: value
     });
   };
 
+
   render() {
     return (
-      <div className="container">
-        <h1>Login</h1>
-        <form onSubmit={this.handleFormSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email address:</label>
-            <input className="form-control"
-                   placeholder="Email goes here..."
-                   name="email"
-                   type="email"
-                   id="email"
-                   onChange={this.handleChange}/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="pwd">Password:</label>
-            <input className="form-control"
-                   placeholder="Password goes here..."
-                   name="password"
-                   type="password"
-                   id="pwd"
-                   onChange={this.handleChange}/>
-          </div>
-          <button type="submit" className="btn btn-secondary">Submit</button>
-        </form>
-        <p><Link to="/signup">Go to Signup</Link></p>
+      <div className = "row" style={{width:"90w", margin:"20px"}}>
+
+      <div className="col-7">
+        <img src="https://s.yimg.com/ny/api/res/1.2/8p2O7gZMPgItJCGzD.zB5g--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/http://media.zenfs.com/en-GB/homerun/motor1_uk_340/f1359b01e9e71cdd35a81f3217cbde2e"/>
+        </div>
+
+        <div className="col-5">
+          <h1>Login</h1>
+          <form onSubmit={this.handleFormSubmit}>
+            <div className="form-group">
+              <label htmlFor="email">Email address:</label>
+              <input className="form-control"
+                placeholder="Email goes here..."
+                name="email"
+                type="email"
+                id="email"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="pwd">Password:</label>
+              <input className="form-control"
+                placeholder="Password goes here..."
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={this.handleChange} />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+          <p><Link to="/signup">Go to Signup</Link></p>
+        </div>
+     
       </div>
 
     );
   }
 }
+
 
 export default Login;
